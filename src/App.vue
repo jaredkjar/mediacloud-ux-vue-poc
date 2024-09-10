fe1b81a03c6fd1652d39e0cf3a8975d88a9e4827
jared <jaredkjar@gmail.com>
2024-09-09 19:05:54 -0600

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App, friend!"/>
 </template>
 
 <script>
@@ -22,5 +22,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
