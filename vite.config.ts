1fa10bad42374129861f2af3d43365c82555e048
jared <jaredkjar@gmail.com>
2024-09-16 16:14:33 -0600

    conver to vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.js
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vitePluginSingleSpa from "vite-plugin-single-spa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vitePluginSingleSpa({
+      serverPort: 3005,
+      spaEntryPoints: "src/main.tsx",
+    }),
+  ],
+});
